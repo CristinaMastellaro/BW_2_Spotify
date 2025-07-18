44d6158248f4598820e44ced736aed79b469331c
Cristina Mastellaro <cristinamastellaro8@gmail.com>
2025-07-18 10:14:45 +0200

    wip

--- scripts/search.js
+++ scripts/search.js
@@ -116,7 +116,7 @@ const showResults = (endpointToUse) => {
             }
             if (k !== resultsSearch.data.length) {
               artistiSection.innerHTML += `
-              <div><a class=" rounded-circle text-decoration-none text-light" href="./artist.html?id=${resultsSearch.data[i].artist.id}">
+              <div><a class=" rounded-circle text-decoration-none text-light" href="./artist.html?id=${resultsSearch.data[k].artist.id}">
                 <img src="${resultsSearch.data[k].artist.picture_medium}" alt="" class="rounded-circle"/>
                 <div>
                   <h5>${resultsSearch.data[k].artist.name}</h5>
